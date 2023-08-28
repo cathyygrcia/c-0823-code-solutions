5ac729b3660340e7f9e5df12b5c7d56912828d54
Cathy Garcia <garciacathy67@yahoo.com>
2023-08-28 19:19:20 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+const fullName = 'Cathy Garcia';
+const isCool = true;
+const totalPets = 1;
+const unknown = null;
+let never;
+
+console.log('Value of fullName: ', fullName);
+console.log('Value of isCool: ', isCool);
+console.log('Value of totalPets: ', totalPets);
+console.log('Value of unknown: ', unknown);
+console.log('Value of never: ', never);
+console.log('typeof fullName: ', typeof fullName);
+console.log('typeof isCool: ', typeof isCool);
+console.log('typeof totalPets: ', typeof totalPets);
+console.log('typeof unknown: ', typeof unknown);
+console.log('typeof never: ', typeof never);

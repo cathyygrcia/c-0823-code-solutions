9caf4f414158ce26f23f63eeb66b302ef24304e5
Cathy Garcia <garciacathy67@yahoo.com>
2023-09-04 20:22:54 +0000

    Completed array-manipulation-2 exercise!

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,10 @@
 /* exported chunk */
+function chunk(array, size) {
+  const chunked = [];
+  for (let i = 0; i < array.length; i += size) {
+    const chunk = array.slice(i, i + size);
+    chunked.push(chunk);
+  }
+
+  return chunked;
+}

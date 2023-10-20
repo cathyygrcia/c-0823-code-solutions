692274fbce0b8290cf847506a8a764227040abb1
Cathy Garcia <garciacathy67@yahoo.com>
2023-10-20 21:21:48 +0000

    Completed express-delete exercise.

---
+++ express-delete/express-delete-quiz-notes.md
@@ -0,0 +1,37 @@
+# express-delete-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What is the significance of an HTTP request's method?
+
+  it defines the action that should be taken on the resource and how the server should process the request.
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
---
+++ express-delete/server.ts
@@ -0,0 +1,55 @@
+import express from 'express';
+
+const app = express();
+
+type Grade = {
+  id: number;
+  name: string;
+  course: string;
+  score: number;
+};
+
+const grades: Record<number, Grade> = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Håkon Wium Lie',
+    course: 'CSS',
+    score: 92,
+  },
+};
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray: Grade[] = [];
+
+  for (const key in grades) {
+    gradesArray.push(grades[key]);
+  }
+  res.json(gradesArray);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  const gradeId = parseInt(req.params.id);
+
+  if (grades[gradeId]) {
+    delete grades[gradeId];
+    res.sendStatus(204);
+  } else {
+    res.status(404).json({ error: 'Grade not found' });
+  }
+});
+
+app.listen(8080, () => {
+  console.log('Server is running on port', 8080);
+});
